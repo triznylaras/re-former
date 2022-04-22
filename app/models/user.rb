093449cb29efc2a27220f8e5eb22a7f819d2cd0b
triznylaras <triznylaras@gmail.com>
2022-04-22 22:06:34 +0700

    create validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 4..14 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
 end
