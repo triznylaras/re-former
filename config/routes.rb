5c4dd668c0c4aefa9fe42057f2bc67b93aec6422
triznylaras <triznylaras@gmail.com>
2022-06-02 17:33:04 +0700

    create edit form function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create edit update]
 end
